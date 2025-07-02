1d691862b1a9d871bc4b01a7a4c94f2b9f1a7017
mohdfaizanmalik <faizanmalik6575@gmail.com>
2025-07-02 16:33:28 +0530

    i changh code

--- script.js
+++ script.js
@@ -348,9 +348,46 @@
 // console.log(c);    // Answer = false
 
 
-// 10. Logical AND/OR:
+//                                10. Logical AND/OR:
 //  Create two boolean variables, x and y. Use the && operator to combine x and y,
 // and the || operator to combine x and y. Log the results of both operations.
+//  let a = true;
+//  let b = false;
+//  let c = a && b;
+//  console.log(c);
+                                     
+
+                              //     control flow
+
+// 1. Write code that checks if the variable `num` is greater than 10. If it is, print "Greater than 10".
+// let num = 15; // You can change this value to test
+// 2. Write code that checks if the variable `num` is greater than or equal to 0. If it is, print
+// "Non-negative"; otherwise, print "Negative".
+// let num = -3; // You can change this value to test
+// 3. Write code that checks the variable `score` and prints a grade based on the following criteria:
+// - "Excellent" for scores 90 and above
+// - "Good" for scores between 70 and 89
+// - "Needs Improvement" for scores below 70
+// let score = 85; // You can change this value to test
+// 4. Write code that uses a `switch` statement to print the name of the day based on the variable
+// `day`. Use the following values:
+// - 1 for "Monday"
+// - 2 for "Tuesday"
+// - 3 for "Wednesday"
+// - 4 for "Thursday"
+// - 5 for "Friday"
+// - 6 for "Saturday"
+// - 7 for "Sunday"
+// let day = 4; // You can change this value to test
+// 5. Write code that checks if the variable `num` is between 10 and 20 (inclusive). Print "In range"
+// if it is, otherwise print "Out of range".
+// let num = 15; // You can change this value to test
+// 6. Write code that checks if the variable `num` is positive. If it is, then check if it is greater than
+// 100. Print "Large Positive" if it is greater than 100; otherwise, print "Small Positive".
+// let num = 120; // You can change this value to test
+// 7.Write code that checks if the variable `num` is even or odd. Print "Even" if it is even, and
+// "Odd" if it is odd.
+// let num = 7; // You can change this value to test    
 
 
 
@@ -395,67 +432,5 @@
 
 
 
-
-
-
-
-
-// var number = prompt("enter a number");
-// console.log("number :",number)
-// if(number > 0){
-//           console.log(`${number} is positive`)
-// }
-// else if (number < 0){
-//           console.log("this is a negativ number")
-// }
-// else{
-//           console.log("this give num is ziro");
-// }
-
-// console.log("this is next line");
-
-
-// var number = prompt("enter a number");
-// console.log("number=",number);
-// if(number >= 0){
-//           // console.log(`${number} is positive`);
-//          if(number==0){
-//           console.log("the number is ziro");
-
-//                  }
-//                else{
-//           console.log("the number is positive");
-// } 
-// }
-// else{
-//           console.log("the number is nagative")
-// }
-
-
-// var number = prompt("Enter the even number");
-// if(number % 2 == 0){
-//           console.log("this is a even number");
-// }
-// else{
-//           console.log("this is a odd number");
-// }
-
-                    //      switch 
-var a = Number(prompt("Enter number(1,2,3)"));
-switch(a){
-          case 1:
-                    console.log("one");
-                    break;
-          case 2:
-                    console.log("two");
-                    break;
-          case 3:
-                    console.log("three");
-                    break;
-          default:
-                    console.log("not a valid number");
-                    break;
-                                                                                 
-}                    
 
 
