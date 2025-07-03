315a565a09338cb1ca6da2f0eafe569c42ee4c43
mohdfaizanmalik <faizanmalik6575@gmail.com>
2025-07-03 17:40:37 +0530

    i changhe code

--- control-flow/control-flow.js
+++ control-flow/control-flow.js
@@ -29,7 +29,12 @@
 // - "Needs Improvement" for scores below 70
 // let score = 85; // You can change this value to test
 
-
+// let num = 90;
+// if(num >= 90){
+//           console.log("good");
+// }else{
+//           console.log("needs Improvement");  // Answer = good
+// }
 
 // 4. Write code that uses a `switch` statement to print the name of the day based on the variable
 // `day`. Use the following values:
@@ -42,20 +47,69 @@
 // - 7 for "Sunday"
 // let day = 4; // You can change this value to test
 
+// var day = Number(prompt("Enter the day(1,2,3,4,5,6,7)"));
+// switch(day){
+//           case 1:
+//                     console.log("Monday");
+//                     break;
+//           case 2:
+//                     console.log("Tuesday");
+//                     break;
+//           case 3:
+//                     console.log("wednesday");
+//                     break;
+//           case 4:
+//                     console.log("thursday");
+//                     break;
+//           case 5:
+//                     console.log("friday");
+//                     break;
+//           case 6:
+//                     console.log("saturday");
+//                     break;
+//           case 7:
+//                     console.log("sunday");
+//                     break;
+//           default: 
+//                     console.log("not a valid value");                             
+
+// }
 
 
 // 5. Write code that checks if the variable `num` is between 10 and 20 (inclusive). Print "In range"
 // if it is, otherwise print "Out of range".
 // let num = 15; // You can change this value to test
 
+// let num = 4;
+// if(num < 20){
+//           console.log("In range");
+// }
+// else if(num > 10){
+//           console.log("out of range");
+// }
 
+// else{
+          
+// }
 
 // 6. Write code that checks if the variable `num` is positive. If it is, then check if it is greater than
 // 100. Print "Large Positive" if it is greater than 100; otherwise, print "Small Positive".
 // let num = 120; // You can change this value to test
 
-
+// let num = 120;
+// if(num > 100){
+//           console.log("big positive");
+// }else{
+//           console.log("small negative");  // Answer = big positive
+// }
 
 // 7.Write code that checks if the variable `num` is even or odd. Print "Even" if it is even, and
 // "Odd" if it is odd.
 // let num = 7; // You can change this value to test    
+
+// let num = 9;
+// if(num % 2 === 0){
+//           console.log("Even")
+// }else{
+//           console.log("odd")   // Answer = odd
+// }
