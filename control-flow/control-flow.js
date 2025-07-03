b33701d1c9ca6d6fbddb2d9830b12ca55beb60e8
mohdfaizanmalik <faizanmalik6575@gmail.com>
2025-07-03 12:42:42 +0530

    i changhe code

---
+++ control-flow/control-flow.js
@@ -0,0 +1,61 @@
+                           //     control flow
+
+// 1. Write code that checks if the variable `num` is greater than 10. If it is, print "Greater than 10".
+// let num = 15; // You can change this value to test
+ 
+// let num = 15;
+// if(num > 10){
+//           console.log("greater than 10");
+// }
+// else{
+//           console.log(" LESS then 10");    // Answer = greater than 10
+// }
+
+// 2. Write code that checks if the variable `num` is greater than or equal to 0. If it is, print
+// "Non-negative"; otherwise, print "Negative".
+// let num = -3; // You can change this value to test 
+
+// let num = -3;
+// if(num <= 0){
+//           console.log("positive")
+// }
+// else{
+//           console.log("negative")    // Answer = positive
+// }
+
+// 3. Write code that checks the variable `score` and prints a grade based on the following criteria:
+// - "Excellent" for scores 90 and above
+// - "Good" for scores between 70 and 89
+// - "Needs Improvement" for scores below 70
+// let score = 85; // You can change this value to test
+
+
+
+// 4. Write code that uses a `switch` statement to print the name of the day based on the variable
+// `day`. Use the following values:
+// - 1 for "Monday"
+// - 2 for "Tuesday"
+// - 3 for "Wednesday"
+// - 4 for "Thursday"
+// - 5 for "Friday"
+// - 6 for "Saturday"
+// - 7 for "Sunday"
+// let day = 4; // You can change this value to test
+
+
+
+// 5. Write code that checks if the variable `num` is between 10 and 20 (inclusive). Print "In range"
+// if it is, otherwise print "Out of range".
+// let num = 15; // You can change this value to test
+
+
+
+// 6. Write code that checks if the variable `num` is positive. If it is, then check if it is greater than
+// 100. Print "Large Positive" if it is greater than 100; otherwise, print "Small Positive".
+// let num = 120; // You can change this value to test
+
+
+
+// 7.Write code that checks if the variable `num` is even or odd. Print "Even" if it is even, and
+// "Odd" if it is odd.
+// let num = 7; // You can change this value to test    
